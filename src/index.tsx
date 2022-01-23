1b763d8c0cddb1eeef5936ade94cb41997c1c260
aratama <16192627+aratama@users.noreply.github.com>
2022-01-23 21:29:07 +0900

    add link

--- src/index.tsx
+++ src/index.tsx
@@ -38,73 +38,82 @@ const App = () => {
   }, []);
 
   return c_table ? (
-    <div style={{ display: "flex" }}>
-      <div>
-        <h2>RGB</h2>
+    <div>
+      <div style={{ display: "flex" }}>
         <div>
-          {new Array(10).fill(0).map((_, i) => {
-            const c1 = hexToRgb(color1);
-            const c2 = hexToRgb(color2);
-            const mixed = rgbToHex(rgbMix(c1, c2, 0.1 * i));
-            return (
-              <div
-                key={i}
-                style={{
-                  width: "200px",
-                  height: "60px",
-                  backgroundColor: mixed,
-                  textShadow: "0px 0px 5px white, 0px 0px 5px white, 0px 0px 5px white, 0px 0px 5px white",
-                  lineHeight: "60px",
-                }}
-              >
-                {mixed}
-              </div>
-            );
-          })}
+          <h2>RGB</h2>
+          <div>
+            {new Array(10).fill(0).map((_, i) => {
+              const c1 = hexToRgb(color1);
+              const c2 = hexToRgb(color2);
+              const mixed = rgbToHex(rgbMix(c1, c2, 0.1 * i));
+              return (
+                <div
+                  key={i}
+                  style={{
+                    width: "200px",
+                    height: "60px",
+                    backgroundColor: mixed,
+                    textShadow: "0px 0px 5px white, 0px 0px 5px white, 0px 0px 5px white, 0px 0px 5px white",
+                    lineHeight: "60px",
+                  }}
+                >
+                  {mixed}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <h2>MixBox</h2>
+          <div>
+            {new Array(10).fill(0).map((_, i) => {
+              const c1 = hexToRgb(color1);
+              const c2 = hexToRgb(color2);
+              const mixed = rgbToHex(mixbox_lerp_srgb8(c_table, c1, c2, 0.1 * i));
+              return (
+                <div
+                  key={i}
+                  style={{
+                    width: "200px",
+                    height: "60px",
+                    backgroundColor: mixed,
+                    textShadow: "0px 0px 5px white, 0px 0px 5px white, 0px 0px 5px white, 0px 0px 5px white",
+                    lineHeight: "60px",
+                  }}
+                >
+                  {mixed}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div
+          style={{
+            padding: "100px",
+            display: "flex",
+            gap: "50px",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <input id="color1" type="color" value={color1} onChange={(e) => setColor1(e.target.value)} />
+            {color1}
+          </div>
+          <div>
+            <input id="color2" type="color" value={color2} onChange={(e) => setColor2(e.target.value)} />
+            {color2}
+          </div>
         </div>
       </div>
-      <div>
-        <h2>MixBox</h2>
-        <div>
-          {new Array(10).fill(0).map((_, i) => {
-            const c1 = hexToRgb(color1);
-            const c2 = hexToRgb(color2);
-            const mixed = rgbToHex(mixbox_lerp_srgb8(c_table, c1, c2, 0.1 * i));
-            return (
-              <div
-                key={i}
-                style={{
-                  width: "200px",
-                  height: "60px",
-                  backgroundColor: mixed,
-                  textShadow: "0px 0px 5px white, 0px 0px 5px white, 0px 0px 5px white, 0px 0px 5px white",
-                  lineHeight: "60px",
-                }}
-              >
-                {mixed}
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div
-        style={{
-          padding: "100px",
-          display: "flex",
-          gap: "50px",
-          flexDirection: "column",
-          justifyContent: "space-between",
-        }}
-      >
-        <div>
-          <input id="color1" type="color" value={color1} onChange={(e) => setColor1(e.target.value)} />
-          {color1}
-        </div>
-        <div>
-          <input id="color2" type="color" value={color2} onChange={(e) => setColor2(e.target.value)} />
-          {color2}
-        </div>
-      </div>
+
+      <p>
+        This is a JavaScript porting of <a href="https://github.com/scrtwpns/pigment-mixing">pigment-mixing</a>.
+      </p>
+      <p>
+        <a href="https://github.com/aratama/pigment-mixing">Source Codes</a>.
+      </p>
     </div>
   ) : (
     <p>Loading</p>
